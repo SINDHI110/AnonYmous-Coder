a31a07abf707db6327fb83242caee81674ff65e9
SINDHI110 <80647158+SINDHI110@users.noreply.github.com>
2021-08-12 17:32:46 +0500

    Add files via upload

---
+++ Zphisher.sh
@@ -0,0 +1,55 @@
+GNU nano 5.8 zphisher.sh
+#!/bin/bash
+
+## Zphisher : Automated Phishing Tool
+## Author : TAHMID RAYAT
+## Version : 2.1
+## Github : https://github.com/htr-tech
+.v v. ..v.mm.mm m.. vc
+## THANKS TO :
+## Aditya Shakya - https://github.com/adi1090x
+## Moises Tapia - https://github.com/MoisesTapia
+## TheLinuxChoice - https://twitter.com/linux_chovcb
+ e
+## DarksecDevelopers - https://github.com/DarksecDevelopers
+## Ignitetch - https://github.com/Ignitetch/AdvPhishing
+## Võ Ngọc Bảo - https://github.com/vongocbao
+## Mustakim Ahmed - https://www.facebook.com/Learn.Termux.009
+
+
+## If you Copy Then Give the credits :)
+
+## GNU GENERAL PUBLIC LICENSE
+## Version 3, 29 June 2007
+##
+## Copyright (C) 2007 Free Software Foundation, Inc. 
+## Everyone is permitted to copy and distribute verbatim copies
+## of this license document, but changing it is not allowed.
+##
+## Preamble
+##
+## The GNU General Public License is a free, copyleft license for
+## software and other kinds of works.
+##
+## The licenses for most software and other practical works are designed
+## to take away your freedom to share and change the works. By contrast,
+## the GNU General Public License is intended to guarantee your freedom to
+## share and change all versions of a program--to make sure it remains free
+## software for all its users. We, the Free Software Foundation, use the
+## GNU General Public License for most of our software; it applies also to
+## any other work released this way by its authors. You can apply it to
+## your programs, too.
+##
+## When we speak of free software, we are referring to freedom, not
+## price. Our General Public Licenses are designed to make sure that you
+## have the freedom to distribute copies of free software (and charge for
+## them if you wish), that you receive source code or can get it if you
+## want it, that you can change the software or use pieces of it in new
+## free programs, and that you know you can do these things.
+##
+## To protect your rights, we need to prevent others from denying you
+## these rights or asking you to surrender the rights. Therefore, you have
+[ Read 678 lines ]
+^G Help ^O Write Out ^W Where Is ^K Cut ^T Execute ^C Location M-U Undo
+^X Exit ^R Read File ^\ Replace ^U Paste ^J Justify ^/ Go To Line M-E Redo
+
